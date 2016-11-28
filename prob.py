3600a1935fa72e0383410434bd00a6501384fcbe
SolomonGebreyohannes <solomongyc@gmail.com>
2016-11-28 00:06:17 -0500

    prob.py

---
+++ prob.py
@@ -0,0 +1,37 @@
+import numpy as np
+import collections
+import scipy.stats as stats 
+import matplotlib.pyplot as plt 
+
+data = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 4, 4, 4, 4, 5, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 9, 9]
+
+c = collections.Counter(data)
+count_sum = sum(c.values())
+
+# Frequency 
+for k,v in c.iteritems():
+  print("The frequency of number " + str(k) + " is " + str(float(v) / count_sum))
+
+# Boxplot  
+plt.boxplot(data)
+plt.savefig("boxplot.png") 
+
+# Histogram  
+plt.hist(data, histtype='bar')
+plt.savefig("histogramplot.png") 
+
+## QQ-plot 
+stats.probplot(data, dist="norm", plot=plt) 
+plt.savefig("qqplot.png")
+ 
+
+
+
+
+
+
+
+
+
+
+
