8e86b87d1f0e8cc550c9a7f416f4b4b4ef2232f3
SolomonGebreyohannes <solomongyc@gmail.com>
2016-12-08 17:01:59 -0500

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,36 +1,37 @@
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm 
+import matplotlib.pyplot as plt 
 
 # Load the data 
 loansData = pd.read_csv('https://github.com/Thinkful-Ed/curric-data-001-data-sets/raw/master/loans/loansData.csv')  
 
+# Clean and extract columns 
 # Drop null rows
-loansData.dropna(inplace=True) 
- 
-loansData['Interest.Rate'] = [float(interest[0:-1])/100 for interest in loansData['Interest.Rate']] 
-loansData['Loan.Length'] = [int(length[0:-7]) for length in loansData['Loan.Length']] 
-loansData['FICO.Score'] = [int(val.split('-')[0]) for val in loansData['FICO.Range']] 
-
-# Extract columns 
-intrate = loansData['Interest.Rate']
+loansData.dropna(inplace=True)
+# Remove '%' and covert to number  
+intrate = [float(value.rstrip('%')) for value in loansData['Interest.Rate']]
 loanamt = loansData['Amount.Requested']
-fico = loansData['FICO.Score']  
+# Split the range and take the first number
+fico = [int(val.split('-')[0]) for val in loansData['FICO.Range']]  
 
 # Reshape the data 
 y = np.matrix(intrate).transpose()
 x1 = np.matrix(fico).transpose()
 x2 = np.matrix(loanamt).transpose() 
-  
+   
 # Input matrix 
 x = np.column_stack([x1,x2]) 
- 
+
 # Model 
 X = sm.add_constant(x)
 model = sm.OLS(y,X)
 f = model.fit() 
 
-# Summary  
-f.summary() 
+# Summary and plot 
+print f.summary() 
+plt.scatter(fico, intrate, color='red') 
+plt.show()
+
  
 
