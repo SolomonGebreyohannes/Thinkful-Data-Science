99098b232908508b8c90e5b044192cc495695368
SolomonGebreyohannes <solomongyc@gmail.com>
2016-12-24 11:29:59 -0500

    Update prob_lending_club.py

--- prob_lending_club.py
+++ prob_lending_club.py
@@ -1,7 +1,9 @@
 """
 Challenge
 
-Write a script called "prob_lending_club.py" that reads in the loan data, cleans it, and loads it into a pandas DataFrame. Use the script to generate and save a boxplot, histogram, and QQ-plot for the values in the "Amount.Requested" column. Be able to describe the result and how it compares with the values from the "Amount.Funded.By.Investors".
+Write a script called "prob_lending_club.py" that reads in the loan data, cleans it, and loads it into a pandas DataFrame. 
+Use the script to generate and save a boxplot, histogram, and QQ-plot for the values in the "Amount.Requested" column. 
+Be able to describe the result and how it compares with the values from the "Amount.Funded.By.Investors".
 """
 
 import matplotlib.pyplot as plt 
