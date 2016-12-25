82e9c28cb680b0bb5a250f85ca5111d9fe57ca74
SolomonGebreyohannes <solomongyc@gmail.com>
2016-12-25 13:06:39 -0500

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -5,41 +5,41 @@ import matplotlib.pyplot as plt
 import math 
  
 def logistic_function(coeff, ficoScore, loanAmount): 
-	p = [1.0/(1.0 + math.exp(1.0 + (coeff*fc)-(0.000174*loanAmount))) for fc in ficoScore]    
-	return p 
-   
-def pred(p):
-	if p>=0.70:
-		decision = 1.0  
+	p = 1.0/(1.0 + math.exp(coeff[0] + (coeff[1]*ficoScore) + (coeff[2]*loanAmount)))
+	# p = 1.0/(1.0 + math.exp(-coeff[0] - (coeff[1]*ficoScore) - (coeff[2]*loanAmount)))    
+	return p     
+
+def pred(p):  
+	if p >= 0.7:
+		decision = 1.0
 	else:
-		decision = 0.0 
-	return decision    
+		decision = 0.0  
+	return decision
 
 # Load the data 
 loansData = pd.read_csv('https://github.com/Thinkful-Ed/curric-data-001-data-sets/raw/master/loans/loansData.csv')  
 
 # Clean and add columns 
-# Drop null rows
 loansData.dropna(inplace=True)
-# Remove '%' and convert to number  
 loansData['Interest.Rate'] = [float(value.rstrip('%')) for value in loansData['Interest.Rate']] 
-# FicoScore - split the range and take the first number
 loansData['FicoScore'] = [int(val.split('-')[0]) for val in loansData['FICO.Range']] 
-# Add IR_TF 
 loansData['IR_TF'] = [1 if value >= 12 else 0 for value in loansData['Interest.Rate']]     
-# Add ind_vars
-loansData['ind_vars'] = 1.0 
+loansData['Intercept'] = 1.0 
+ind_vars = ['Intercept', 'FicoScore', 'Amount.Requested']
 
 # Logistic regression 
-logit = sm.Logit(loansData['IR_TF'], loansData['ind_vars'])  
+logit = sm.Logit(loansData['IR_TF'], loansData[ind_vars])  
 result = logit.fit()
-# print result.summary() 
-coeff = result.params
+coeff = result.params  
 
+# Plot  
 loanAmount = 10000
-prob = logistic_function(coeff, loansData['FicoScore'], loanAmount)   
-# print(coeff)  
+fScore = [ficoScore for ficoScore in range(550,950)]
+prob = [logistic_function(coeff,ficoScore,loanAmount) for ficoScore in fScore]
+plt.plot(fScore, prob, 'green') 
 
-plt.plot(loansData['FicoScore'], prob)  
-plt.show() 
-    
+dec_index = [fs for fs in range(550,950,5)]
+dec = [pred(logistic_function(coeff,fs,loanAmount)) for fs in dec_index] 
+plt.scatter(dec_index, dec, color='red', s=5) 
+
+plt.show()
