63d61fa3ef76af9b0c498ea409d951502198cfeb
SolomonGebreyohannes <solomongyc@gmail.com>
2017-04-25 23:11:01 -0400

    Update multivariate.py

--- multivariate.py
+++ multivariate.py
@@ -1,26 +1,35 @@
 import pandas as pd
 import numpy as np
-import statsmodels.api as sm
+import statsmodels.api as sm 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # Load the Lending Club Statistics 
 loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
 
 int_rate = [float(value.rstrip('%')) for value in loansData['Interest.Rate']]
 annual_inc = loansData['Monthly.Income']
-fico = [int(val.split('-')[0]) for val in loansData['FICO.Range']]
+fico = [int(val.split('-')[0]) for val in loansData['FICO.Range']] 
+loansData['home_ownership'] = pd.Categorical(loansData['Home.Ownership']).codes
+hm_ownshp = loansData['home_ownership'] 
  
 # Use income (annual_inc) to model interest rates (int_rate)   
 y = np.matrix(int_rate).transpose()
 x1 = np.matrix(fico).transpose()
-x2 = np.matrix(annual_inc).transpose()
+x2 = np.matrix(annual_inc).transpose()  
 
 x = np.column_stack([x1,x2])
 
 X = sm.add_constant(x)
-model = sm.OLS(y,X)
+model = sm.OLS(y,X,missing='drop')
 f = model.fit() 
-
-print f.summary() 
+print f.summary()
 
 # Add home ownership (home_ownership) to the model 
-# TODO   
+x3 = np.matrix(hm_ownshp).transpose()  
+x = np.column_stack([x1,x2,x3])
+
+X = sm.add_constant(x)
+model = sm.OLS(y,X,missing='drop')
+f = model.fit() 
+print f.summary() 
